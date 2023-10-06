160afcf2c86cf10bb4cc69aadfd6d985161c6e6b
djjohnsongeek <danieleejohnson@gmail.com>
2023-10-05 21:42:07 -0400

    feat: adjusting info and home page detials

--- prizesApp/controllers/sweepstakes_controller.py
+++ prizesApp/controllers/sweepstakes_controller.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 sweepstakes_blueprint = Blueprint("sweepstakes", __name__)
 from prizesApp.forms import RegisterForm
+from prizesApp.repo import appRepo
 
 @sweepstakes_blueprint.route("/sweepstakes/<int:sweepstakes_id>", methods=["GET"])
 def info(sweepstakes_id: int):
-    return render_template("sweepstakes/info.html")
+    sweepstakes = appRepo.retrieve_sweepstake(sweepstakes_id)
+
+    if sweepstakes is None:
+        flash("Sweepstakes not found :(", "danger")
+        return redirect(url_for("index.home"))
+
+    return render_template("sweepstakes/info.html", sweepstakes=sweepstakes)
 
 @sweepstakes_blueprint.route("/sweepstakes/register/<int:sweepstakes_id>", methods=["GET", "POST"])
 def register(sweepstakes_id: int):
