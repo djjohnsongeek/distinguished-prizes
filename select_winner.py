fae10015f61a28322b1734f54e6323551963a75e
Daniel <danieleejohnson@gmail.com>
2023-12-15 08:04:15 -0500

    feat: more logic for winner selection script

--- select_winner.py
+++ select_winner.py
@@ -1,7 +1,10 @@
 
 from app.models.database import *
 from instance.config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
-from app.services import email_service
+from app.services import email_service, log_service
+from datetime import datetime
+from peewee import MySQLDatabase, fn
+import uuid
 
 db_models = [User, Sweepstake, Participant, Entry, Winner, LoginLog, Post, PageView]
 
@@ -12,12 +15,13 @@ def select_winners():
     print("Testing winner selection!")
 
     # Get all Sweepstakes ready for selection
-        # end date must be less than now()
-        # filter out any sweepstaks that already have winnser (confirmed our not)
-    # Grab random entries with the sweepstakes id
-    # Grab participant information for that entry
-    # Create winner entry
-    # Send email confirmation to winner
+    sweepstakes = Sweepstake.select().where(Sweepstake.end_date <= datetime.now())
+    for sweepstake in sweepstakes:
+        if len(sweepstakes.winners) > 0:
+            winning_participant = get_random_winner(sweepstake)
+            guid = generate_confirmation_uuid()
+            result = create_winner(sweepstake, winning_participant, guid)
+                # Send email confirmation to winner
 
     # Check current winners to see if any are unconfirmed and expired
     # if expired, grab a new random entry
@@ -41,6 +45,38 @@ def get_db():
         model.bind(db)
 
     return db
+
+def create_winner(sweepstake: Sweepstake, participant: Participant, guid: str) -> bool:
+    new_id = 0
+    try:
+        new_id = Winner.insert(
+            participant = participant,
+            sweepstake = sweepstake,
+            selection_date = datetime.now(),
+            confirmation_guid = guid,
+            confirmation_date = None,
+            fullfilled = False,
+            fullfilled_date = None,
+            firstname = None,
+            lastname = None,
+            address1 = None,
+            address2 = None,
+            city = None,
+            state = None,
+            zipcode = None,
+        ).execute()
+    except Exception as e:
+        log_service.log_error("Failed to selecte a winner for sweepstake", "select_winner.py", { "winner_id": participant.id, "sweepstake_id": sweepstake.id, "e_msg": str(e)})
+        pass
+
+    return new_id > 0
+
+def generate_confirmation_uuid() -> str:
+    return str(uuid.uuid4())
+
+def get_random_winner(sweepstake_id: int) -> Participant:
+    selected_entry = Entry.select().join(Participant).where(Entry.sweepstake.id == sweepstake_id).order_by(fn.Random()).limit(1)
+    return selected_entry.participant
 
 if __name__ == '__main__':
     select_winners()
